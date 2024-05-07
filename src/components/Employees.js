3e56607045034d521e539302a4c353e5adca6bf1
abeni <abinesilew@gmail.com>
2024-05-07 13:23:36 -0700

    React Commit

---
+++ src/components/Employees.js
@@ -0,0 +1,22 @@
+const Employees = [
+    {
+        id:"1",
+        Name:"Abeni Sileshi",
+        Age:"30",
+        Gender:"Male"
+    }
+    ,{
+        id: "2",
+        Name: "Kaleba Compa",
+        Age: "45",
+        Gender:"Male"
+    },
+    {
+        id:"3",
+        Name:"Redi Lje",
+        Age:"20",
+        Gender:"Female"
+    }
+]
+
+export default Employees
